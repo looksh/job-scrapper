dbd3a441e2f6bf6c5c7166ae42e675540d6d90fe
mooonu <lookshdev@gmail.com>
2024-01-09 17:45:57 +0900

    페이지 요소 가져오는 방법

--- main.py
+++ main.py
@@ -5,11 +5,26 @@ url = "https://weworkremotely.com/categories/remote-full-stack-programming-jobs#
 
 response = requests.get(url)
 
-soup = BeautifulSoup(response.content, "html.parser",)
+soup = BeautifulSoup(response.content, "html.parser", )
 
 # class_를 사용하는 이유는 python에서 class는 예약어이므로
 jobs = soup.find("section", class_="jobs").find_all("li")[1: -1]
 
+all_jobs = []
+
 for job in jobs:
-    title = job.find("span", class_="title")
-    print(title)
+    title = job.find("span", class_="title").text
+    company, time, region = job.find_all("span", class_="company")
+    url = job.find("div", class_="tooltip")
+    if url:
+        url = url.next_sibling["href"]
+    job_data = {
+        "title": title,
+        "company": company.text,
+        "time": time.text,
+        "region": region.text,
+        "url": f"https://weworkremotely.com{url}",
+    }
+    all_jobs.append(job_data)
+
+print(all_jobs)
