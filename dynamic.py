3751ad71e22b5bf836938f4edf59c0c418ac6b0c
mooonu <lookshdev@gmail.com>
2024-01-13 17:39:00 +0900

    playwright 이용 wanted 제어 사랑니 이슈로 오늘도 휴식 ㅜ

--- dynamic.py
+++ dynamic.py
@@ -1,4 +1,5 @@
 from playwright.sync_api import sync_playwright
+import time
 
 pw = sync_playwright().start()
 
@@ -7,6 +8,32 @@ browser = pw.chromium.launch(headless=False)
 
 page = browser.new_page()
 
-page.goto("https://example.com/")
+page.goto("https://www.wanted.co.kr/jobsfeed")
 
-page.screenshot(path="screenshot.png")
+time.sleep(1)
+
+page.click("button.Aside_searchButton__Xhqq3")
+
+time.sleep(1)
+
+page.get_by_placeholder("검색어를 입력해 주세요.").fill("python")
+
+time.sleep(1)
+
+page.keyboard.down("Enter")
+
+time.sleep(1)
+
+page.click("a#search_tab_position")
+
+time.sleep(3)
+
+page.keyboard.down("End")
+
+time.sleep(1)
+
+page.keyboard.down("End")
+
+time.sleep(3)
+
+pw.stop()
